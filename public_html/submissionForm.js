80b8a40fac18683a4d9d492753cad7e61c3fe46f
tomgsmith99 <tomgsmith99@gmail.com>
2015-12-02 09:52:42 -0500

    can read input form after randomization

--- public_html/submissionForm.js
+++ public_html/submissionForm.js
@@ -8,7 +8,7 @@ function getQSvaluesForm() {
         name = "value" + i;
         thisform += "<input class='input' type='text' size='2' id='" + name + "'>";
     }
-    thisform += "<button type = 'button' id = 'submitValues'>run</button>";
+    thisform += "<button type = 'button' id = 'submitValues' onclick='run()'>run</button>";
     thisform += "<button type = 'button' id = 'randButton' onclick='randomize()'>randomize</button>";
 
     thisform += "</form>";
@@ -24,4 +24,17 @@ function randomize() {
         id = "value" + i;
         document.getElementById(id).value = Math.floor(Math.random() * 100);
     }
+}
+
+function run() {
+    A =[];
+    var i;
+    var id;
+    
+    for (i=0; i<=9; i++){
+        id = "value" + i;
+        A.push(document.getElementById(id).value);
+    }
+    
+    console.log("The main array is now: " + A);
 }
