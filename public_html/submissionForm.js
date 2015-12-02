ea4698fa8a3d3ecc729a9d0f92ca9da73064b270
tomgsmith99 <tomgsmith99@gmail.com>
2015-12-01 23:51:23 -0500

    more UI progress. Randomize button does something.

--- public_html/submissionForm.js
+++ public_html/submissionForm.js
@@ -1,14 +1,27 @@
 
 function getQSvaluesForm() {
-    var thisform = "<form name='initialArray'>";
+    var thisform = "<form id='qsArray' name='qsArray'>";
     var name = "";
     var i;
 
     for (i = 0; i <= 9; i++) {
         name = "value" + i;
-        thisform += "<input type='text' size='2' name='" + name + "'>";
+        thisform += "<input class='input' type='text' size='2' id='" + name + "'>";
     }
-    
+    thisform += "<button type = 'button' id = 'submitValues'>run</button>";
+    thisform += "<button type = 'button' id = 'randButton' onclick='randomize()'>randomize</button>";
+
     thisform += "</form>";
+    
     return thisform;
+}
+
+function randomize() {
+    var i;
+    var id;
+    
+    for (i=0; i<=9; i++){
+        id = "value" + i;
+        document.getElementById(id).value = 6;
+    }
 }
